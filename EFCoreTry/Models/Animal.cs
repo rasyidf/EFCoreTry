b4265e2426735fb93fcc0d16cb55ae3fb4a128fa
Muhammad Fahmi Rasyid <rasyid.ufa@gmail.com>
2021-06-03 02:45:00 +0700

    Add Animal Model

---
+++ EFCoreTry/Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTry.Models
+{
+    public class Animal
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Breed> Breed { get; set; }
+        public string DateBorn { get; set; }
+        public Gender Gender { get; set; }
+        public bool Registered { get; set; }
+        public string Color { get; set; }
+        public string Photo { get; set; }
+    }
+}
---
+++ EFCoreTry/Models/Breed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTry.Models
+{
+    public class Breed
+    {
+        public string Name { get; set; }
+        public Category Category { get; set; }
+    }
+}
